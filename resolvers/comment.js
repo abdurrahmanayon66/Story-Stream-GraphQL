7d83c1d6965d9bd3ce27b8fa4369b99614a821df
Abdur Rahman Ayon <abdurrahmanayon66@gmail.com>
2025-06-08 05:32:28 +0600

    Created a new schema for commnets and fixed the resolvers for commenst

--- resolvers/comment.js
+++ resolvers/comment.js
@@ -1,30 +1,87 @@
-// resolvers/comment.js
 const { parseResolveInfo } = require("graphql-parse-resolve-info");
 
 module.exports = {
+  Query: {
+    commentsByBlogId: async (_, { blogId }, { prisma }, info) => {
+      const resolveInfo = parseResolveInfo(info);
+      const fields = resolveInfo?.fieldsByTypeName?.Comment || {};
+
+      const comments = await prisma.comment.findMany({
+        where: { blogId },
+        orderBy: { createdAt: "asc" },
+        include: {
+          user: !!fields.user,
+          parentComment: !!fields.parentCommentId,
+        },
+      });
+
+      return comments.map((comment) => ({
+        ...comment,
+        user: comment.user
+          ? {
+              ...comment.user,
+              image:
+                fields.user?.image && comment.user.image
+                  ? Buffer.from(comment.user.image).toString("base64")
+                  : null,
+              profileImage: fields.user?.profileImage
+                ? comment.user.profileImage
+                : null,
+            }
+          : null,
+      }));
+    },
+  },
   Mutation: {
-      createComment: async (_, { blogId, content }, { user, prisma }, info) => {
+    createComment: async (
+      _,
+      { blogId, content, parentCommentId },
+      { user, prisma },
+      info
+    ) => {
       if (!user) throw new Error("Unauthorized");
 
       const blog = await prisma.blog.findUnique({ where: { id: blogId } });
       if (!blog) throw new Error("Blog not found");
 
+      if (parentCommentId) {
+        const parent = await prisma.comment.findUnique({
+          where: { id: parentCommentId },
+        });
+        if (!parent) throw new Error("Parent comment not found");
+      }
+
       const comment = await prisma.comment.create({
         data: {
           content,
           blogId,
-          authorId: user.id,
+          userId: user.id,
+          parentCommentId: parentCommentId || null,
         },
       });
 
       const resolveInfo = parseResolveInfo(info);
-      const fields = resolveInfo.fieldsByTypeName.Comment || {};
+      const fields = resolveInfo?.fieldsByTypeName?.Comment || {};
 
-      let author = null;
+      const likeCount = fields.likeCount
+        ? await prisma.commentLike.count({ where: { commentId: comment.id } })
+        : 0;
+
+      const hasLiked = fields.hasLiked
+        ? !!(await prisma.commentLike.findFirst({
+            where: {
+              commentId: comment.id,
+              userId: user.id,
+            },
+          }))
+        : false;
+
+      let userData = null;
       let blogData = null;
-      if (fields.author) {
-        author = await prisma.user.findUnique({
-          where: { id: comment.authorId },
+
+      if (fields.user) {
+        userData = await prisma.user.findUnique({
+          where: { id: comment.userId },
         });
       }
       if (fields.blog) {
@@ -35,15 +92,15 @@ module.exports = {
 
       return {
         ...comment,
-        author: author
+        user: userData
           ? {
-              ...author,
+              ...userData,
               image:
-                fields.author?.image && author.image
-                  ? Buffer.from(author.image).toString("base64")
+                fields.user?.image && userData.image
+                  ? Buffer.from(userData.image).toString("base64")
                   : null,
-              profileImage: fields.author?.profileImage
-                ? author.profileImage
+              profileImage: fields.user?.profileImage
+                ? userData.profileImage
                 : null,
             }
           : null,
@@ -55,7 +112,57 @@ module.exports = {
                 : null,
             }
           : null,
+        likeCount,
+        hasLiked,
       };
+    },
+
+    deleteComment: async (_, { commentId }, { user, prisma }) => {
+      if (!user) throw new Error("Unauthorized");
+
+      const comment = await prisma.comment.findUnique({
+        where: { id: commentId },
+      });
+
+      if (!comment) throw new Error("Comment not found");
+      if (comment.userId !== user.id)
+        throw new Error("Not authorized to delete this comment");
+
+      await prisma.comment.delete({ where: { id: commentId } });
+      return true;
+    },
+
+    toggleCommentLike: async (_, { commentId }, { user, prisma }) => {
+      if (!user) throw new Error("Unauthorized");
+
+      const existingLike = await prisma.commentLike.findUnique({
+        where: {
+          commentId_userId: {
+            commentId,
+            userId: user.id,
+          },
+        },
+      });
+
+      if (existingLike) {
+        await prisma.commentLike.delete({
+          where: {
+            commentId_userId: {
+              commentId,
+              userId: user.id,
+            },
+          },
+        });
+        return false;
+      } else {
+        await prisma.commentLike.create({
+          data: {
+            commentId,
+            userId: user.id,
+          },
+        });
+        return true;
+      }
     },
   },
 };
--- schemas/comment.js
+++ schemas/comment.js
@@ -1,15 +1,37 @@
 const { gql } = require('graphql-tag');
 
 module.exports = gql`
-  type Comment {
+type Comment {
+  id: Int!
+  content: String!
+  blogId: Int!
+  userId: Int!
+  parentCommentId: Int
+  createdAt: DateTime!
+  blog: Blog
+  user: User
+  parentComment: Comment
+  replies: [Comment!]!
+  likeCount: Int!
+  hasLiked: Boolean!
+}
+
+  type CommentLike {
     id: Int!
-    content: String!
-    blog: Blog!
+    commentId: Int!
+    userId: Int!
+    comment: Comment!
     user: User!
     createdAt: DateTime!
   }
 
+  extend type Query {
+    commentsByBlogId(blogId: Int!): [Comment!]!
+  }
+
   extend type Mutation {
-    createComment(blogId: Int!, content: String!): Comment!
+    createComment(blogId: Int!, content: String!, parentCommentId: Int): Comment!
+    deleteComment(commentId: Int!): Boolean!
+    toggleCommentLike(commentId: Int!): Boolean!
   }
 `;
